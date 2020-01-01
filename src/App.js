4fb05471da9fc92ebf73690b7d2860bec84b3432
JessicaNovo <50515278+JessicaNovo@users.noreply.github.com>
2020-01-01 18:28:54 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Users App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Users App</h1>
+      </div>
+    </div>
+  );
 }
