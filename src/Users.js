33a10c2d9ed5e963223e99026a48b970eab94f4a
JessicaNovo <50515278+JessicaNovo@users.noreply.github.com>
2020-01-02 12:15:58 +0000

    Installed axios and displayed list of users

--- src/Users.js
+++ src/Users.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Users.css";
 
 export default function Users() {
-  return (
-    <div className="Users">
-      <div class="row">
-        <div class="col-sm-4">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Name</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Company Name</h6>
+  function handleData(response) {
+    setUserData(response.data.slice(0, 10));
+    setReady(true);
+  }
+
+  const [userData, setUserData] = useState("");
+  const [ready, setReady] = useState(false);
+
+  if (ready) {
+    return (
+      <div className="Users row">
+        {userData.map(function(user) {
+          return (
+            <div className="col-sm-4 user-cards">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{user.name}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {user.company.name}
+                  </h6>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Phone: {user.phone}</li>
+                  <li className="list-group-item">Email: {user.email}</li>
+                </ul>
+                <div className="card-body">
+                  <a href=" " className="card-link">
+                    Read more
+                  </a>
+                </div>
+              </div>
             </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Phone: </li>
-              <li class="list-group-item">Email: </li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">
-                Read more
-              </a>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-    </div>
-  );
+    );
+  } else {
+    const usersUrl = "https://jsonplaceholder.typicode.com/users";
+    axios.get(usersUrl).then(handleData);
+    return <div>Loading...</div>;
+  }
 }
